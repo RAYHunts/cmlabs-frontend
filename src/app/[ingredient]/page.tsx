e8056486d916b0a1ea4c698a1e135b9babfdd5b7
RAYHunts <rayhunts710@gmail.com>
2024-01-19 08:21:18 +0700

    fix error on detail

--- src/app/[ingredient]/page.tsx
+++ src/app/[ingredient]/page.tsx
@@ -1,7 +1,4 @@
 import { getMealsByIngredient } from "@/services/apiRequest";
-import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 import MealList from "./MealList";
 
 const Ingredient = async ({ params }: { params: { ingredient: string } }) => {
--- src/app/meal/[mealId]/page.tsx
+++ src/app/meal/[mealId]/page.tsx
@@ -26,7 +26,7 @@ const DetailMealPage = async ({ params }: { params: { mealId: string } }) => {
       <div>
         <h3 className="text-xl font-semibold">Ingredients</h3>
         <ul className="mt-4 list-disc list-inside">
-          {Object.keys(data).map((key) => {
+          {/* {Object.keys(data).map((key) => {
             if (key.includes("strIngredient") && data[key]) {
               return (
                 <li key={key}>
@@ -35,7 +35,28 @@ const DetailMealPage = async ({ params }: { params: { mealId: string } }) => {
               );
             }
           }
-          )}
+          )} */}
+          {data.strMeasure1 && <li>{data.strMeasure1} - {data.strIngredient1}</li>}
+          {data.strMeasure2 && <li>{data.strMeasure2} - {data.strIngredient2}</li>}
+          {data.strMeasure3 && <li>{data.strMeasure3} - {data.strIngredient3}</li>}
+          {data.strMeasure4 && <li>{data.strMeasure4} - {data.strIngredient4}</li>}
+          {data.strMeasure5 && <li>{data.strMeasure5} - {data.strIngredient5}</li>}
+          {data.strMeasure6 && <li>{data.strMeasure6} - {data.strIngredient6}</li>}
+          {data.strMeasure7 && <li>{data.strMeasure7} - {data.strIngredient7}</li>}
+          {data.strMeasure8 && <li>{data.strMeasure8} - {data.strIngredient8}</li>}
+          {data.strMeasure9 && <li>{data.strMeasure9} - {data.strIngredient9}</li>}
+          {data.strMeasure10 && <li>{data.strMeasure10} - {data.strIngredient10}</li>}
+          {data.strMeasure11 && <li>{data.strMeasure11} - {data.strIngredient11}</li>}
+          {data.strMeasure12 && <li>{data.strMeasure12} - {data.strIngredient12}</li>}
+          {data.strMeasure13 && <li>{data.strMeasure13} - {data.strIngredient13}</li>}
+          {data.strMeasure14 && <li>{data.strMeasure14} - {data.strIngredient14}</li>}
+          {data.strMeasure15 && <li>{data.strMeasure15} - {data.strIngredient15}</li>}
+          {data.strMeasure16 && <li>{data.strMeasure16} - {data.strIngredient16}</li>}
+          {data.strMeasure17 && <li>{data.strMeasure17} - {data.strIngredient17}</li>}
+          {data.strMeasure18 && <li>{data.strMeasure18} - {data.strIngredient18}</li>}
+          {data.strMeasure19 && <li>{data.strMeasure19} - {data.strIngredient19}</li>}
+          {data.strMeasure20 && <li>{data.strMeasure20} - {data.strIngredient20}</li>}
+          
         </ul>
       </div>
       <div>
